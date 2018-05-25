4198dde4419a7e69277db70cfc6b6a0768fbddf9
DESKTOP-OBQ3MS0\caleb <caleb.ray.williams@gmail.com>
2018-05-24 21:29:51 -0500

    Initial

---
+++ RizzleDizzle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RizzleDizzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class RunescapeProcessService
+    {
+
+    }
+
+    public class RunescapeKeyboardInputService
+    {
+
+    }
+}
